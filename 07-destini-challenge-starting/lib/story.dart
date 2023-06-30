52c999ada8c4c073770a19e4521bd18d2caea91a
Bishoywadea <108888519+Bishoywadea@users.noreply.github.com>
2023-07-01 02:57:47 +0300

    destini Done👨🏻‍💻

---
+++ 07-destini-challenge-starting/lib/story.dart
@@ -0,0 +1,14 @@
+class Story {
+  String storyTitle = 'a';
+  String choice1 = 'a';
+  String choice2 = 'a';
+
+  Story(
+      {String this.storyTitle = "g",
+      String this.choice1 = "g",
+      String this.choice2 = "g"}) {
+    storyTitle = storyTitle;
+    choice1 = choice1;
+    choice2 = choice2;
+  }
+}
